3c8c59060e8758c5b477835e539e69a262e7c47b
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-06-27 20:53:52 -0300

    Fetch do arquivo JSON

--- js/index.js
+++ js/index.js
@@ -1,20 +1,12 @@
-    const data = [{
-    "nome":"giovanni",
-    "img": "pizzamaos.jpg",
-    "sobrenome":"neves"
-},
-{
-    "nome":"Jorge",
-    "img": "foto2.jpeg",
-    "sobrenome":"Sadauscas Filho"
-},
-{
-    "nome":"Gabriel",
-    "img": "foto1.jpeg",
-    "sobrenome":"Neves"
-}];
+fetch("/js/pizzas.json")
+    .then((rawData) => {
+        return rawData.json()
+    })
+    .then(data => {
+        console.log(data)
 
-for(var i = 0; i < data.length; i++) {
-    document.getElementById("teste").innerHTML += (`<p>${data[i].nome} ${data[i].sobrenome} </p>`)
-    document.getElementById("teste").innerHTML += (`<img src="/imgs/${data[i].img}"/>`)
-    }
+        for (var i = 0; i < data.length; i++) {
+            document.getElementById("teste").innerHTML += (`<p>${data[i].nome} ${data[i].sobrenome} </p>`)
+            document.getElementById("teste").innerHTML += (`<img src="/imgs/${data[i].img}"/>`)
+        }
+    })
