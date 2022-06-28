f8c304f8b8cf7deaf2353a916f739e5fde6b8f21
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2022-06-27 22:16:58 -0300

    Melhorando o JSON Javascript para adicionar os produtos

--- js/index.js
+++ js/index.js
@@ -3,10 +3,24 @@ fetch("/js/pizzas.json")
         return rawData.json()
     })
     .then(data => {
-        console.log(data)
+        data.forEach(data => {
+            document.getElementById("produtos").innerHTML += (`
+            <div class="col-md-4">
+                <div class="card m-2">
+                    <img src="/imgs/${data.img}" class="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">${data.titulo}</h5>
+                        <p class="card-text">${data.descrição}</p>
+                        <hr />
+                        <h5 class="card-title">Preços</h5>
+                        <span class="fw-lighter small">${data.aviso}</span>
+                        <p class="card-text">Unidade - R$${data.preços.unidade}</p>
+                        <p class="card-text">Pacote com ${data.preços.quantidade_pacote1} - R$${data.preços.pacote1}</p>
+                        <p class="card-text">Pacote com ${data.preços.quantidade_pacote2} - R$${data.preços.pacote2}</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        });
 
-        for (var i = 0; i < data.length; i++) {
-            document.getElementById("teste").innerHTML += (`<p>${data[i].nome} ${data[i].sobrenome} </p>`)
-            document.getElementById("teste").innerHTML += (`<img src="/imgs/${data[i].img}"/>`)
-        }
     })
